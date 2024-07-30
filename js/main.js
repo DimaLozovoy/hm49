a9ee7a04200e879539caaad748e8c78e0f516cf8
DimaLozovoy <112244746+DimaLozovoy@users.noreply.github.com>
2024-07-30 15:50:29 +0300

    ended hm

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let age = 15;
+console.log(age);
+let firstName = "Dimachka";
+console.log(firstName);
+let = isStudent = true;
+console.log(isStudent);
+const myString = "Когда меня рожали... Собвстевнно, тогда я и родился";
+console.log(myString);
+let myNumber = 1478;
+myNumber += 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+alert("У тебе є можливість вввести дату коли Team Spirit дізбанднуться");
+let DateOfSpiritsEnd = prompt("Введіть дату:");
+alert(`Чорт, Team Spirit дізбанднуться: ${DateOfSpiritsEnd}`);
+let text;
